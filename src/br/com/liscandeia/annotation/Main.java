90606c9fe4842a15434234748b7fe17a2cbd129d
liscandeia <liscandeia523@gmail.com>
2024-05-16 15:10:50 -0300

    reflections

--- src/br/com/liscandeia/annotation/Main.java
+++ src/br/com/liscandeia/annotation/Main.java
@@ -1,7 +1,16 @@
 package br.com.liscandeia.annotation;
 
+import javax.xml.namespace.QName;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Class<?> clazz = ClassAnnotation.class;
+
+        Table tableAnnotation = clazz.getAnnotation(Table.class);
+        String nomeDaTabela = tableAnnotation.value();
+
+        System.out.println("Nome da tabela: " + nomeDaTabela);
+
     }
 }
